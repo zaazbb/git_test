5b82110ce065ffbe45d96ef5a4b2402c12e33014
zaazbb <zaazbb@163.com>
2016-08-16 10:13:52 +0800

    '------------------------------------------------------------------------

--- git_test.py
+++ git_test.py
@@ -8,7 +8,7 @@ git_path = r'C:\Users\jf\AppData\Local\Atlassian\SourceTree\git_local\bin'
 
 
 s = b'------------------------------------------------------------------------\r\nr550 | lvfangfang | 2016-07-29 10:26:24 +0800 (\xd6\xdc\xce\xe5, 29 \xc6\xdf\xd4\xc2 2016) | 1 line\r\n\r\n\xd4\xf6\xbc\xd3HIF_SPI\xd0\xad\xd2\xe92 (\xce\xb4\xcd\xea\xb4\xfd\xd0\xf8\xa3\xa9\r\n------------------------------------------------------------------------\r\n'
-log = s.decode('gbk')
+log = s.decode('gbk').replace('\r', '')
 
 os.environ['PATH'] += ';' + git_path
 run('git add -A', shell=True)
